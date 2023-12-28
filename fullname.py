e6fdb8617f82802fa199b8cca06e6d00af6a1754
ndmichael <ukejemicheal@gmail.com>
2023-12-28 22:59:10 +0100

    first step

---
+++ fullname.py
@@ -0,0 +1,6 @@
+f_name = "ada"
+l_name = "lovelace"
+full_name = f"{f_name} {l_name}".title()
+msg = f"Hello, {full_name}"
+
+print(msg)
---
+++ hellow_world.py
@@ -0,0 +1,5 @@
+msg = "Hello World!"
+print(msg)
+
+msg = "Hello Python Crash Course World!"
+print(msg)
