47be1655507d043ea7e6988a5c887646bf59cbcd
ndmichael <ukejemicheal@gmail.com>
2023-12-28 23:32:28 +0100

    string cases trials

---
+++ Chpt2/strings_cases.py
@@ -0,0 +1,15 @@
+# cases trials, quotes
+
+name = "Tupac Amaru Shakur"
+msg = "Should I just stop trying and give up?  But then, that's exactly what they're waiting for me to do."
+print(f'{name}, I love you forever.')
+
+# upper
+print(f'{name.upper()}')
+# lower
+print(f'{name.lower()}')
+# title
+print(f'{name.title()}')
+
+# msg
+print(f"{name} once said, '{msg}'")
