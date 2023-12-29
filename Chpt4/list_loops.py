622cd302058107f555c912d83fb5131ee36cf2e2
ndmichael <ukejemicheal@gmail.com>
2023-12-29 12:50:05 +0100

    looping through list

---
+++ Chpt4/list_loops.py
@@ -0,0 +1,6 @@
+soups = ['egusi', 'oha', 'ogbonno', 'okra', 'bitter leaf', 'white soup']
+
+for soup in soups:
+    print(f"I love {soup.title()} soup.")
+
+print("I really do love soups with different swallow.")
