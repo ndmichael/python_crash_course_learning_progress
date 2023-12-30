5ffb44cf9b48f4f37248b2433ef86240a6855334
ndmichael <ukejemicheal@gmail.com>
2023-12-30 14:28:55 +0100

    slicing

--- Chpt4/list_loops.py
+++ Chpt4/list_loops.py
@@ -4,3 +4,12 @@ for soup in soups:
     print(f"I love {soup.title()} soup.")
 
 print("I really do love soups with different swallow.")
+
+print("First 3 items in the list are? ")
+print(soups[:3])
+
+print("Middle 3 items in the list are? ")
+print(soups[2:])
+
+print("Last 3 items in the list are? ")
+print(soups[-3:])
