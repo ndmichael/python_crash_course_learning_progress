086bbe04e7691d2b882710eed6e7e07cfb314fac
ndmichael <ukejemicheal@gmail.com>
2024-01-04 17:54:16 +0100

    function trial

---
+++ Chpt8/greet.py
@@ -0,0 +1,8 @@
+def msg():
+    print("I am learning Python crash course edition 3.")
+
+def favourite_book(title):
+    print(f"My favourite book is, {title.title()}")
+
+msg()
+favourite_book("'The mafia manager'.")
