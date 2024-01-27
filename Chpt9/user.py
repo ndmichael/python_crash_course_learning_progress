b2e7f352f476fd8097fdca5669f85573dca9bb90
ndmichael <ukejemicheal@gmail.com>
2024-01-27 15:33:59 +0100

    update the class to include login features

--- Chpt9/user.py
+++ Chpt9/user.py
@@ -4,6 +4,7 @@ class User:
         self.l_name = l_name
         self.desig = desig
         self.age = age
+        self.login_attempt = 0
 
     def describe_user(self):
         print("**** DESCRIBE USER ****")
@@ -15,7 +16,24 @@ class User:
     def greet_user(self):
         print(f"you are welcome, {self.f_name} {self.l_name}.")
 
+    def increment_login_attempt(self):
+        self.login_attempt += 1
+
+    def reset_login_attempt(self):
+        self.login_attempt = 0
+
 
 user1 = User("Michael", "Ukeje", "Software Engineer", 31)
+user2 = User("Daniela", "Petrova", "Hacker ethical", 29)
 user1.describe_user()
 user1.greet_user()
+
+print()
+user2.increment_login_attempt()
+user2.increment_login_attempt()
+user2.increment_login_attempt()
+user2.increment_login_attempt()
+print(f"attempts: {user2.login_attempt}")
+user2.reset_login_attempt()
+
+print(f"attempts after reset: {user2.login_attempt}")
