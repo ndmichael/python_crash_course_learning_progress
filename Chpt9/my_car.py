f025b710596f8ed1e1579c47bd60ced2a5483d48
ndmichael <ukejemicheal@gmail.com>
2024-05-29 21:32:21 +0100

    updates added

--- Chpt9/my_car.py
+++ Chpt9/my_car.py
@@ -1,26 +1,5 @@
-from car import Car
-class Battery:
-    def __init__(self, battery_size=40):
-        self.battery_size = battery_size
+from car import Car, ElectricalCar
 
-    def describe_battery_life(self):
-        """Describing battery life when called"""
-        print(f"this car have a battery of {self.battery_size} km/h")
-
-    def range(self):
-        if self.battery_size == 40:
-            range = 150
-        elif self.battery_size == 65:
-            range = 250
-
-        print(f"This car can go about {range} miles on a full charge.")
-
-class ElectricalCar(Car):
-    def __init__(self, make, model, year):
-        """Initialize attributes of the parent class"""
-        super().__init__(make, model, year)
-        self.battery_size = 40
-        self.battery = Battery()
 
     
 
