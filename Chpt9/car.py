2b5b0eb7e5588979c17fa09db82fee2a83c9afd8
ndmichael <ukejemicheal@gmail.com>
2024-05-29 21:31:50 +0100

    battery and electricalcar classes added

--- Chpt9/car.py
+++ Chpt9/car.py
@@ -1,3 +1,5 @@
+"""The car classes for both gas and electric car"""
+
 class Car:
     """Representing a car."""
     def __init__(self, make, model, year):
@@ -28,12 +30,25 @@ class Car:
         self.odometer_reading += miles
 
 
+class Battery:
+    def __init__(self, battery_size=40):
+        self.battery_size = battery_size
+
+    def describe_battery_life(self):
+        """Describing battery life when called"""
+        print(f"this car have a battery of {self.battery_size} km/h")
+
+    def range(self):
+        if self.battery_size == 40:
+            range = 150
+        elif self.battery_size == 65:
+            range = 250
+
+        print(f"This car can go about {range} miles on a full charge.")
+
 class ElectricalCar(Car):
     def __init__(self, make, model, year):
         """Initialize attributes of the parent class"""
         super().__init__(make, model, year)
         self.battery_size = 40
-
-
-my_e1 = ElectricalCar('nissan', 'leaf', '2024')
-print(my_e1.get_descriptive_name())
+        self.battery = Battery()
