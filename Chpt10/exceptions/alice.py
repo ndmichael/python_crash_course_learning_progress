14e9c2e43641199a361c1a57361a3fd0b9a1b405
ndmichael <ukejemicheal@gmail.com>
2024-06-08 14:27:36 +0100

    put into a function\

--- Chpt10/exceptions/alice.py
+++ Chpt10/exceptions/alice.py
@@ -1,12 +1,17 @@
 from pathlib import Path
 
 path = Path("alice.txt")
-try:
-    content = path.read_text(encoding='utf-8')
-    
-except:
-    print("File not found.")
-else:
-    lines = content.split()
-    total_words  = len(lines)
-    print(f"The file {path} has about {total_words} words")
+
+def count_word(path):
+    """Code to words in a file"""
+    try:
+        content = path.read_text(encoding='utf-8')
+        
+    except:
+        print("File not found.")
+    else:
+        lines = content.split()
+        total_words  = len(lines)
+        print(f"The file {path} has about {total_words} words")
+
+count_word(path)
