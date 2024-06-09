4b922d7cb7545c7550d1df5a0585b770ac3a7dc8
ndmichael <ukejemicheal@gmail.com>
2024-06-09 18:29:54 +0100

    working test including middlename

--- Chpt11/test_name_function.py
+++ Chpt11/test_name_function.py
@@ -4,3 +4,8 @@ def test_first_last_name():
     """Do names like janis and joplin work"""
     formatted_name = get_formatted_name("janis", "joplins")
     assert formatted_name == "Janis Joplins"
+
+def test_first_last_middle_name():
+    """Do you like Wolgang Amadeus Mozart"""
+    formatted_name = get_formatted_name("wolfgang", "mozart", "amadeus")
+    assert formatted_name == "Wolfgang Amadeus Mozart"
