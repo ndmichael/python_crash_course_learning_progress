c525b9ae63c36bde967c12843fcdab44b36154a7
ndmichael <ukejemicheal@gmail.com>
2024-06-09 18:59:58 +0100

    add test case to test for population

--- Chpt11/exercises/test_cities.py
+++ Chpt11/exercises/test_cities.py
@@ -1,8 +1,13 @@
 from city_func import city_country
 
-
 def test_city_country():
-    "is Lagos, Nigeria overhyped"
+    "is Lagos Nigera overhyped?"
     location = city_country('lagos', 'nigeria')
-
     assert location == "Lagos, Nigeria"
+
+
+def test_city_country_population():
+    "is Kano more populated than lagos?"
+    location = city_country('kano', 'nigeria', "11,000,000")
+
+    assert location == "Kano, Nigeria - Population 11,000,000"
