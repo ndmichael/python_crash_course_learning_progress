80365a245edef585c2623b78cf98a3177ffdcf18
ndmichael <ukejemicheal@gmail.com>
2024-06-09 18:24:58 +0100

    adjust function to collect optional username

--- Chpt11/name_func.py
+++ Chpt11/name_func.py
@@ -1,6 +1,9 @@
-def get_formatted_name(fname: str, lname: str)-> str:
+def get_formatted_name(fname: str, lname: str, middle: str='')-> str:
     """Generate a neatly formatted fullname"""
-    fullname = f"{fname} {lname}"
+    if middle:
+        fullname = f"{fname} {middle} {lname}"
+    else:
+        fullname = f"{fname} {lname}"
     return fullname.title()
 
 
