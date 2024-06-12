93a3d9c7547916566dab06ca20bc0404ad95ab1f
ndmichael <ukejemicheal@gmail.com>
2024-06-12 12:39:45 +0100

    using pytest fixtures

--- Chpt11/test_survey.py
+++ Chpt11/test_survey.py
@@ -1,15 +1,23 @@
+import pytest
 from survey import AnonymousSurvey
 
-def test_store_single_response():
+
+@pytest.fixture
+def language_survey():
+    '''would be available for all test functions'''
+    question = "What language did you first learn?"
+    lansuage_survey = AnonymousSurvey(question)
+    
+    return language_survey
+
+def test_store_single_response(language_survey):
     '''Testing that single response is being stored.'''
-    language_survey = AnonymousSurvey("What language did you first learn?")
     language_survey.store_responses("english")
     assert "English" in language_survey.responses
 
 
-def test_store_three_responses():
+def test_store_three_responses(language_survey):
     '''Test to check storing more than one response'''
-    language_survey = AnonymousSurvey("What language did you first learn?")
     responses = ["English", "Igbo", "Yoruba"]
     for res in responses:
         language_survey.store_responses(res)
