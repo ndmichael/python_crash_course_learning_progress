e23b23bc156c9109ec4c2cfca0925b266d44fcdd
ndmichael <ukejemicheal@gmail.com>
2024-06-12 12:33:50 +0100

    to test for multiple answers

--- Chpt11/test_survey.py
+++ Chpt11/test_survey.py
@@ -5,3 +5,15 @@ def test_store_single_response():
     language_survey = AnonymousSurvey("What language did you first learn?")
     language_survey.store_responses("english")
     assert "English" in language_survey.responses
+
+
+def test_store_three_responses():
+    '''Test to check storing more than one response'''
+    language_survey = AnonymousSurvey("What language did you first learn?")
+    responses = ["English", "Igbo", "Yoruba"]
+    for res in responses:
+        language_survey.store_responses(res)
+
+    for res in responses:
+        assert res in language_survey.responses
+    
