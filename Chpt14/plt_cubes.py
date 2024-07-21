a7e778d3b17bcc463e16b970b728279f86bcc068
ndmichael <ukejemicheal@gmail.com>
2024-07-21 22:08:43 +0100

    working on cubes

---
+++ Chpt14/plt_cubes.py
@@ -0,0 +1,19 @@
+from matplotlib import pyplot as plt
+
+num_cubes = list(range(1, 5001))
+cubes = [x**3 for x in num_cubes]
+
+plt.plot(num_cubes, cubes, c='green')
+
+#set attributes to plot
+plt.title("My cube graph", fontsize=22)
+plt.xlabel("Cube numbers", fontsize=14)
+plt.ylabel("Cubes values", fontsize=14)
+
+#Set size of tick label
+plt.tick_params(axis="both", labelsize=14)
+
+#Set the range of each axis
+# plt.axis([0, 5100, 0, 5100000])
+
+plt.show()
