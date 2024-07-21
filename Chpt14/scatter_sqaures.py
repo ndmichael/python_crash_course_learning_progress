001253d42b5d370841d573635188354c06a2d5c8
ndmichael <ukejemicheal@gmail.com>
2024-07-21 20:33:27 +0100

    generate squares dynamically

--- Chpt14/scatter_sqaures.py
+++ Chpt14/scatter_sqaures.py
@@ -1,8 +1,9 @@
 from matplotlib import pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
-plt.scatter(x_values, y_values, s=100)
+x_values = list(range(1, 100))
+
+y_values = [x**2 for x in x_values]
+plt.scatter(x_values, y_values, s=40)
 
 plt.title("My Scatter Square", fontsize=24)
 plt.xlabel("Values", fontsize=14)
@@ -11,4 +12,6 @@ plt.ylabel("Squares values", fontsize=14)
 #Set the size of the tick labels
 plt.tick_params(axis="both", which="major", labelsize=14)
 
+#Set the range of each axis
+# plt.axis([0, 110, 0, 110000])
 plt.show()
