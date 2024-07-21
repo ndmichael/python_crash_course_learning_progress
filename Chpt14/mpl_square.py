fa26c14ce3e79c1acef586b001f6f83e5d512fb9
ndmichael <ukejemicheal@gmail.com>
2024-07-21 20:07:04 +0100

    styled the graph to display better

--- Chpt14/mpl_square.py
+++ Chpt14/mpl_square.py
@@ -2,5 +2,14 @@
 from matplotlib import pyplot as plt
 
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares)
+plt.plot(squares, linewidth=5)
+
+#Set chart title and label axis
+plt.title("Square Number", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of value", fontsize=14)
+
+#Set size of tick label
+plt.tick_params(axis="both", labelsize=14)
+
 plt.show()
