019e2daff04bcfd978d5b6f46ef2c345900587f1
ndmichael <ukejemicheal@gmail.com>
2024-07-23 23:50:07 +0100

    created a scatter plot from rabdom work module i created

---
+++ Chpt14/rw_visuals.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as plt
+from random_walk import RandomWalk
+
+#Making a  random walk and plot the points
+rw = RandomWalk()
+rw.fill_walk()
+
+plt.scatter(rw.x_values, rw.y_values, s=15)
+plt.show()
