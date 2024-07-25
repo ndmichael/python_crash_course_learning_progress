cd1f706567564a5033c52beb3cee070467649e6c
ndmichael <ukejemicheal@gmail.com>
2024-07-25 14:21:41 +0100

    emphasizing on the start and end points

--- Chpt14/rw_visuals.py
+++ Chpt14/rw_visuals.py
@@ -9,7 +9,11 @@ while True:
     rw.fill_walk()
 
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers,cmap=plt.cm.Reds,  s=15)
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, s=15)
+
+    #Emphasize the first and last points"
+    plt.scatter(0,0, c="green", edgecolors="none", s=200)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c="purple", edgecolors='red', s=200)
     plt.show()
 
     keep_running = input("Make another walk: ")
