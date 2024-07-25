2027d117c28f24236039cc9d549b483cdd54759d
ndmichael <ukejemicheal@gmail.com>
2024-07-25 14:09:25 +0100

    keep making random walks until the user terminates

--- Chpt14/rw_visuals.py
+++ Chpt14/rw_visuals.py
@@ -1,9 +1,16 @@
 from matplotlib import pyplot as plt
 from random_walk import RandomWalk
 
-#Making a  random walk and plot the points
-rw = RandomWalk()
-rw.fill_walk()
 
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+
+#Making a  random walk and plot the points
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    plt.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+
+    keep_running = input("Make another walk: ")
+    if keep_running in ['n', 'N']:
+        break
