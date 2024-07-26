e2e55f36e38b4755ba61d8979b64fec5819caac0
ndmichael <ukejemicheal@gmail.com>
2024-07-26 15:40:39 +0100

    Analyzig the results

--- Chpt14/die_visual.py
+++ Chpt14/die_visual.py
@@ -8,5 +8,13 @@ for roll_num in range(100):
     result = die.roll()
     results.append(result)
 
-print(results)
-print(results.count(6))
+
+#Analyze the result
+frequencies = {}
+
+for value in range(1, die.num_sides + 1):
+    frequency = results.count(value)
+    frequencies[value] = frequency
+
+
+print(frequencies)
