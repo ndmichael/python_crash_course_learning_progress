a3c38d16c3160caa84a4c9112fb7b6c62eff23ed
ndmichael <ukejemicheal@gmail.com>
2024-07-26 16:18:11 +0100

    added visuals

--- Chpt14/die_visual.py
+++ Chpt14/die_visual.py
@@ -1,10 +1,11 @@
 from die import Die
+import pygal
 
 
 die = Die()
 results = []
 
-for roll_num in range(100):
+for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
@@ -16,5 +17,15 @@ for value in range(1, die.num_sides + 1):
     frequency = results.count(value)
     frequencies[value] = frequency
 
+hist = pygal.Bar()
+hist.force_uri_protocol = 'http'
 
-print(frequencies)
+hist.title = "Results of rolling 1 D6 die 1000 times"
+hist.x_labels = list(frequencies.keys())
+hist.x_title = "Results"
+hist.y_title = "Frequency of Result"
+hist.add('D6', list(frequencies.values()))
+hist.render_to_file('d6_visual.svg', tooltip=True)
+
+
+print(list(frequencies.values()))
