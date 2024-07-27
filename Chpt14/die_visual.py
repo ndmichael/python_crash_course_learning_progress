a3d446678eee5fe04557af9f27db904c2b26d92a
ndmichael <ukejemicheal@gmail.com>
2024-07-27 07:51:01 +0100

    one D6 and D10 dice

--- Chpt14/die_visual.py
+++ Chpt14/die_visual.py
@@ -3,11 +3,11 @@ import pygal
 
 
 die1 = Die()
-die2 = Die()
+die2 = Die(10)
 
 results = []
 
-for roll_num in range(1000):
+for roll_num in range(50000):
     result = die1.roll() + die2.roll()
     results.append(result)
 
@@ -16,21 +16,20 @@ for roll_num in range(1000):
 frequencies = {}
 max_results = die1.num_sides + die2.num_sides
 
-for value in range(1, max_results + 1):
+for value in range(2, max_results + 1):
     frequency = results.count(value)
     frequencies[value] = frequency
 
 
 # Visualize result
 hist = pygal.Bar()
-hist.force_uri_protocol = 'http'
 
-hist.title = "Results of rolling 1 D6 die 1000 times"
+hist.title = "Results of rolling 1 D10 die 10000 times"
 hist.x_labels = list(frequencies.keys())
 hist.x_title = "Results"
 hist.y_title = "Frequency of Result"
-hist.add('D6 + D6', list(frequencies.values()))
-hist.render_to_file('dice_visual.svg', tooltip=True)
+hist.add('D6 + D10', list(frequencies.values()))
+hist.render_to_file('dice_visual10.svg', tooltip=True)
 
 
 print(list(frequencies.values()))
