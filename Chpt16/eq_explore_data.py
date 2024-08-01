f009ff62981d5c2e6d674f589b33e3271562f6b8
ndmichael <ukejemicheal@gmail.com>
2024-08-02 00:29:42 +0100

    magnitue extrated from te data

--- Chpt16/eq_explore_data.py
+++ Chpt16/eq_explore_data.py
@@ -6,6 +6,18 @@ path = Path("eq_data/eq_data_1_day_m1.geojson")
 contents = path.read_text(encoding='utf-8')
 all_eq_data = json.loads(contents)
 
+#Read all data from the dataset
+all_eq_dicts = all_eq_data['features']
+
+#Read mag properties
+mags = []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+
+print(mags[:10])
+print(len(all_eq_dicts))
+
 #Create a more readable versions of the data file
 path = Path("eq_data/readable_eq_data.geojson")
 readable_contents = json.dumps(all_eq_data, indent=4)
