4078de16b77389192416c9aa69c585eca63c2b12
ndmichael <ukejemicheal@gmail.com>
2024-08-03 00:58:33 +0100

    updted to show 2 plotly data

--- Chpt16/eq_explore_data.py
+++ Chpt16/eq_explore_data.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 import json
+import plotly.express as px
 
 #Read data as a string and convert to a python object
-path = Path("eq_data/eq_data_1_day_m1.geojson")
+path = Path("eq_data/eq_data_30_day_m1.geojson")
 contents = path.read_text(encoding='utf-8')
 all_eq_data = json.loads(contents)
 
@@ -20,11 +21,21 @@ for eq_dict in all_eq_dicts:
     lons.append(lon)
     lats.append(lat)
 
-print(mags[:10])
-print(lons[:10])
-print(lats[:10])
-print(len(all_eq_dicts))
+# print(mags[:10])
+# print(lons[:10])
+# print(lats[:10])
+# print(len(all_eq_dicts))
 
+title = "Global Earthquakes"
+fig = px.scatter_geo(
+    lat=lats, lon=lons, size=mags, title=title,
+    color=mags,
+    color_continuous_scale='Viridis',
+    labels={'color': 'Magnitude'},
+    projection='natural earth1'
+
+)
+fig.show()
 #Create a more readable versions of the data file
 path = Path("eq_data/readable_eq_data.geojson")
 readable_contents = json.dumps(all_eq_data, indent=4)
