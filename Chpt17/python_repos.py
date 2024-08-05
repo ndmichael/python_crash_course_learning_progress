2e63e04e1df9c376e6f7142a2ae2025c68d17606
ndmichael <ukejemicheal@gmail.com>
2024-08-05 18:26:34 +0100

    Going further to extract more details

--- Chpt17/python_repos.py
+++ Chpt17/python_repos.py
@@ -11,3 +11,17 @@ print(f"Status code: {r.status_code}")
 # Convert the response object to dictionry
 response_dict = r.json()
 print(*response_dict.keys(), sep="\n")
+
+print(f"Total Repositories: {response_dict['total_count']}")
+print(f"Complete result: {not response_dict['incomplete_results']}")
+
+# Explore inforation about the repository
+repo_dicts = response_dict['item']
+print(f"Repository returned: {len(repo_dicts)}")
+
+# Examine the first repository
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+
+for key in sorted(repo_dict.keys()):
+    print(key, end="\n")
